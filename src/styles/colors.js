7971be2f71bc5a7d128ca8b973bceec89eb4d1ba
Rodrigo Gomes <rodrigo.gomes@liftbank.com.br>
2021-11-06 17:11:36 -0300

    fonts and colors

---
+++ src/styles/colors.js
@@ -0,0 +1,16 @@
+export default {
+  primary: '#F6B20A',
+  secondary: '#14331D',
+  positive: '#49B648',
+  negative: '#E0433C',
+  warning: '#E06C3D',
+
+  white: '#FFFFFF',
+  black: '#2E2E2E',
+  gray1: '#707070', // Descriptions
+  gray2: '#D1D1D6', // Arrow
+  gray3: '#8E8E93', // Values
+  gray4: '#C7C7CC', // Values
+
+  transparent: 'transparent',
+};
---
+++ src/styles/fonts.js
@@ -0,0 +1,22 @@
+export default {
+  size: {
+    font12: 12,
+    font14: 14,
+    font16: 16,
+    font18: 18,
+    font20: 20,
+    font22: 22,
+    font24: 24,
+    font26: 26,
+    font28: 28,
+    font30: 30,
+    font32: 32,
+    font34: 34,
+    font36: 36,
+    font46: 46,
+    font48: 48,
+    title: 22,
+    descrition: 16,
+    button: 18,
+  },
+};
