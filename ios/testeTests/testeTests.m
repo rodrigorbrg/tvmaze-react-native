11a02b750f4b7c192cbebfc1193f9da4273b9089
Rodrigo Gomes <rodrigo.gomes@liftbank.com.br>
2021-11-06 20:39:26 -0300

    full rename project

--- ios/testeTests/testeTests.m
+++ ios/testeTests/testeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface testeTests : XCTestCase
+@interface tvmazeTests : XCTestCase
 
 @end
 
-@implementation testeTests
+@implementation tvmazeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
