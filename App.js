0cda5cd7d0fb48ac4fbaba929307e71b339c902f
Rodrigo Gomes <rodrigo.gomes@liftbank.com.br>
2021-11-06 14:07:27 -0300

    init project

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+ import { useColorScheme } from 'react-native';
+ import { NavigationContainer } from '@react-navigation/native';
+ import { Colors } from 'react-native/Libraries/NewAppScreen';
+ 
+ import Navigator from './src/navigation/Navigator';
+ 
+export const navigationRef = React.createRef();
+
+const App = () => {
+  // const isDarkMode = useColorScheme() === 'dark';
+  // const backgroundStyle = {
+  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // };
+
+  return (
+    <NavigationContainer ref={navigationRef}>
+      <Navigator />
+    </NavigationContainer>
+  );
+};
