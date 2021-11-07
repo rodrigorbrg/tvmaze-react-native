214ad09a4465e20f5c383b082ecc309d97e66008
Rodrigo Gomes <rodrigo.gomes@liftbank.com.br>
2021-11-07 17:28:02 -0300

    episode item ajust

--- src/components/EpisodeItem/styles.js
+++ src/components/EpisodeItem/styles.js
@@ -6,13 +6,13 @@ import fonts from '../../styles/fonts';
 export default styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-    borderBottomColor: colors.gray2,
+    borderBottomColor: colors.gray3,
     borderBottomWidth: 1,
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingBottom: 4,
-    paddingTop: 4,
+    paddingBottom: 8,
+    paddingTop: 8,
   },
   group: {
     flexDirection: 'column',
