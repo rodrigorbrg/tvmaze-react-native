4ad1350bd4e0aef139df752fffae5c837b17ed5e
Rodrigo Gomes <rodrigo.gomes@liftbank.com.br>
2021-11-07 19:45:53 -0300

    iamge episode

--- src/screens/Episode/episode_screen.js
+++ src/screens/Episode/episode_screen.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Dimensions, ScrollView } from 'react-native';
 import { Text, View, Image } from 'react-native';
 
 import { formatDate } from '../../utils/date';
@@ -7,16 +8,32 @@ import styles from './styles';
 
 const Episode = ({ route }) => {
   const [episode, setEpisode] = useState({});
+  const [imageWidth, setImageWidth] = useState();
+  const [imageHeight, setImageHeight] = useState();
 
   useEffect(() => {
-    setEpisode(route.params.episode);
+    const ep = route.params.episode;
+    setEpisode(ep);
+    if (ep?.image?.original) {
+      Image.getSize(ep?.image?.original, (width, height) => {
+        const screenWidth = Dimensions.get('window').width;
+        const scaleFactor = width / screenWidth;
+        const imageHeight = height / scaleFactor;
+        setImageWidth(screenWidth);
+        setImageHeight(imageHeight);
+      });
+    }
   }, []);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.banner}>
+    <ScrollView
+      contentContainerStyle={styles.contentContainer}
+      style={styles.container}
+      scrollEnabled
+    >
+      <View>
         <Image
-          style={styles.image}
+          style={{ width: imageWidth, height: imageHeight }}
           resizeMode={'contain'}
           source={{
             uri: episode?.image?.original,
@@ -27,10 +44,12 @@ const Episode = ({ route }) => {
         <Text style={styles.nameShow}>{episode.name}</Text>
         <Text style={styles.season}>{`Season ${episode?.season}`}</Text>
         <Text style={styles.number}>{`Episode ${episode?.number}`}</Text>
-        <Text style={styles.aired}>{`Aired ${formatDate(episode.airdate)}`}</Text>
+        <Text style={styles.aired}>{`Aired ${formatDate(
+          episode.airdate
+        )}`}</Text>
         <Text style={styles.summary}>{`Summary\n\n${episode?.summary}`}</Text>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
