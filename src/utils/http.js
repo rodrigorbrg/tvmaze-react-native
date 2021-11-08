25ca2b3fdd7f49f6470ffd00fcc1ad45e17c9b18
Rodrigo Gomes <rodrigo.gomes@liftbank.com.br>
2021-11-07 22:40:29 -0300

    cleanup

--- src/utils/http.js
+++ src/utils/http.js
@@ -12,34 +12,28 @@ const defaultHeader = {
 };
 
 const coreGet = async (path) => {
-  console.log(`${appAPI.defaults.baseURL}${path}`);
   try {
     let response = await appAPI.get(`${path}`, defaultHeader);
     return [null, response.data];
   } catch (error) {
-    console.log('error', JSON.stringify(error));
     return [error, null];
   }
 };
 
 const corePatch = async (path, body) => {
   try {
-    console.log(`${appAPI.defaults.baseURL}${path}`, body);
     let response = await appAPI.patch(`${path}`, body, defaultHeader);
     return [null, response.data];
   } catch (error) {
-    console.log('error', JSON.stringify(error));
     return [error, null];
   }
 };
 
 const corePost = async (path, body) => {
   try {
-    console.log(`${appAPI.defaults.baseURL}${path}`, body);
     let response = await appAPI.post(`${path}`, body, defaultHeader);
     return [null, response.data];
   } catch (error) {
-    console.log('error', JSON.stringify(error));
     return [error, null];
   }
 };
