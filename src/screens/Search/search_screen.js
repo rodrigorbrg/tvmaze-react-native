99b693893b38fc07b5976a7a11d3b3620f5fdf98
Rodrigo Gomes <rodrigo.gomes@liftbank.com.br>
2021-11-08 09:31:17 -0300

    load search screen

--- src/screens/Search/search_screen.js
+++ src/screens/Search/search_screen.js
@@ -1,23 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { View, FlatList } from 'react-native';
+import { RefreshControl, View, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import TVMaze from '../../services/tvmaze';
 import SearchBar from '../../components/SeachBar';
 import ShowItem from '../../components/ShowItem';
+import colors from '../../styles/colors';
 
 import styles from './styles';
 
 const Search = () => {
   const [search, setSearch] = useState();
   const [shows, setShows] = useState([]);
+  const [loading, setLoading] = useState(false);
   const navigation = useNavigation();
 
   useEffect(() => {
     if (search) {
       const service = TVMaze();
       const queryShows = async () => {
+        setLoading(true);
         const [err, res] = await service.searchShow(search);
+        setLoading(false);
         if (res) {
           setShows(res);
         }
@@ -42,8 +46,20 @@ const Search = () => {
         data={shows}
         keyExtractor={(item) => item.show.id}
         renderItem={({ item }) => (
-          <ShowItem {...item.show} onPress={() => selectShow(item.show)} iconAction={'add'}/>
+          <ShowItem
+            {...item.show}
+            onPress={() => selectShow(item.show)}
+            iconAction={'add'}
+          />
         )}
+        refreshControl={
+          <RefreshControl
+            refreshing={loading}
+            colors={[colors.primary]}
+            progressBackgroundColor={colors.transparent}
+            tintColor={colors.primary}
+          />
+        }
       />
     </View>
   );
