9180a21598e74a0efb94310160bad680fa1e88cb
Rodrigo Gomes <rodrigo.gomes@liftbank.com.br>
2021-11-08 11:47:16 -0300

    cast from show

---
+++ src/components/Cast/Cast.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { pure } from 'recompose';
+
+import TVMaze from '../../services/tvmaze';
+
+import styles from './styles';
+
+function Cast({ id, name, image }) {
+  const { navigate } = useNavigation();
+
+  const selectPerson = async () => {
+    const service = TVMaze();
+    // const [err, res] = await service.episodeDetails(idPerson);
+    // if (res) {
+    //   navigate('Episode', { episode: res });
+    // }
+  };
+
+  return (
+    <TouchableOpacity key={id} onPress={selectPerson}>
+      <View style={styles.container}>
+        <Image
+          style={styles.image}
+          resizeMode={'contain'}
+          defaultSource={require('../../assets/images/default-movie.png')}
+          source={{
+            uri: image?.medium,
+          }}
+        />
+        <Text style={styles.name}>{name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+export default pure(Cast);
