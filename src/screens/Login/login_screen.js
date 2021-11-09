0494eaa22a6c38d6927bf07c4cd36538449be4b2
Rodrigo Gomes <rodrigo.gomes@liftbank.com.br>
2021-11-09 10:15:00 -0300

    app icons

---
+++ src/screens/Login/login_screen.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import styles from './styles';
+
+const Login = () => {
+  const [pin, setPin] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+  }, []);
+
+  return (
+    <View style={styles.container}>
+  
+    </View>
+  );
+};
+
+export default Login;
---
+++ src/screens/Login/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+import colors from '../../styles/colors';
+
+export default styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.background,
+    flex: 1,
+  },
+});
