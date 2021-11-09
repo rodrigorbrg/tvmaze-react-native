5d8a7d22b2bea64c6abbb921cb568c261dc69278
Rodrigo Gomes <rodrigo.gomes@liftbank.com.br>
2021-11-09 10:00:11 -0300

    redux persist

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,9 +1,18 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import showsReducer from './reducers/shows';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistStore, persistReducer } from 'redux-persist';
+
+const persistConfig = {
+  key: 'storage',
+  storage: AsyncStorage,
+  // whitelist: ['favorities']
+};
 
 const reducers = combineReducers({
-  storage: showsReducer,
+  storage: persistReducer(persistConfig, showsReducer),
 });
 
-export default createStore(reducers, compose(applyMiddleware(thunk)));
+export const store = createStore(reducers, compose(applyMiddleware(thunk)));
+export const persistor = persistStore(store);
